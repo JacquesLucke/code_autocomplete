1691868f34e3e3e31a0e6eafa94adc404d3cdb29
Jacques Lucke <mail@jlucke.com>
2014-11-29 16:12:13 +0100

    TextEditorInfo

---
+++ text_editor_utils.py
@@ -0,0 +1,29 @@
+import bpy
+
+class TextEditorInfo:
+    def __init__(self):
+        context = bpy.context
+        
+        space = context.space_data
+        region = context.region
+        
+        text_block = space.text
+        
+        self.visible_lines = space.visible_lines
+        self.top_line_index = space.top
+        self.text_block = text_block
+        self.line_index = text_block.current_line_index
+        self.character_index = text_block.current_character
+        
+        self.width = region.width
+        self.height = region.height
+        
+        self.line_height = self.height / self.visible_lines
+        self.character_width = self.line_height * 0.436
+        
+        self.visible_line_index = self.line_index - self.top_line_index
+        
+        self.cursor_position = (
+            self.character_width * self.character_index,
+            self.height - self.line_height * self.visible_line_index - self.line_height / 2 )
+        
