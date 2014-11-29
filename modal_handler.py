2a0a57a8389873ae32227e4be9ccea87ca7e3beb
Jacques Lucke <mail@jlucke.com>
2014-11-29 20:43:32 +0100

    changeable draw parameters

--- modal_handler.py
+++ modal_handler.py
@@ -28,17 +28,26 @@ class AutoCompleteTextBox:
     def draw(self):
         scale = self.editor_info.line_height / 20
         
+        line_amount = 8
+        padding = 8 * scale
+        element_height = 21 * scale
+        real_element_height = 20 * scale
+        
         x, y = self.editor_info.cursor_position
-        rectangle = Rectangle(x, y, 200 * scale, 150 * scale)
+        rectangle = Rectangle(x, y, 200 * scale, line_amount * element_height + 2 * padding)
         rectangle.move_down(7 * scale)
     
         draw_rectangle(rectangle)
         draw_rectangle_border(rectangle, thickness = 5 * scale)
         
-        inset = rectangle.get_inset_rectangle(10 * scale)
-        draw_rectangle(inset, color = (0.7, 0.2, 0.2, 1.0))
+        inner_rectangle = rectangle.get_inset_rectangle(padding)
+        
+        elements = self.get_complete_line_amount()[:line_amount]
+        for i, line in enumerate(elements):
+            rectangle = Rectangle(inner_rectangle.left, inner_rectangle.top - i*element_height, inner_rectangle.width, real_element_height)
+            draw_rectangle(rectangle, color = (0.2, 0.3, 0.5, 1.0))
         
         restore_opengl_defaults()
         
-    def getCompleteLines(self):
-        return ["bpy", "Operator", "Panel", "math"]
+    def get_complete_line_amount(self):
+        return ["bpy", "Operator", "Panel", "math", "context", "active_object", "modifiers", "constraints"]
