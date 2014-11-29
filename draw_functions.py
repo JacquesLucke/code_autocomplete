cec84c4306111250ec9ec49780e9c4dce2174287
Jacques Lucke <mail@jlucke.com>
2014-11-29 21:06:53 +0100

    draw text in box

--- draw_functions.py
+++ draw_functions.py
@@ -33,9 +33,12 @@ def draw_rectangle_border(rectangle, thickness = 1, color = (0.1, 0.1, 0.1, 1.0)
     glEnd()
   
 font_id = 1 
+def draw_text_on_rectangle(text, rectangle, color = (0.2, 0.2, 0.2, 1.0), size = 20):
+    draw_text(text, position = (rectangle.left, rectangle.bottom + rectangle.height / 3), size = size, horizontal_align = "LEFT", vertical_align = "BOTTOM")
+
 def draw_text(text = "", position = (0, 0), size = 20, horizontal_align = "CENTER", vertical_align = "BOTTOM", color = (0.2, 0.2, 0.2, 1.0)):
     glColor4f(*color)
-    blf.size(font_id, int(size), 72)
+    blf.size(font_id, int(size), 12)
     dimensions = blf.dimensions(font_id, text)
     
     if horizontal_align == "CENTER":
@@ -43,7 +46,7 @@ def draw_text(text = "", position = (0, 0), size = 20, horizontal_align = "CENTE
     if vertical_align == "TOP":
         position = (position[0], position[1] - dimensions[1])
         
-    blf.position(font_id, position[0], position[1], 0)
+    blf.position(font_id, int(position[0]), int(position[1]), 0)
     blf.draw(font_id, text)
     
     
@@ -51,4 +54,8 @@ def restore_opengl_defaults():
     glLineWidth(1)
     glDisable(GL_BLEND)
     glColor4f(0.0, 0.0, 0.0, 1.0)
+    
+    blf.disable(font_id, blf.ROTATION)
+    blf.disable(font_id, blf.SHADOW)
+    blf.disable(font_id, blf.KERNING_DEFAULT)
     
--- modal_handler.py
+++ modal_handler.py
@@ -32,21 +32,28 @@ class AutoCompleteTextBox:
         padding = 8 * scale
         element_height = 21 * scale
         real_element_height = 20 * scale
+        text_size = 100 * scale
+        border_thickness = 3 * scale
+        
+        selected_index = 3
         
         x, y = self.editor_info.cursor_position
         rectangle = Rectangle(x, y, 200 * scale, line_amount * element_height + 2 * padding)
         rectangle.move_down(7 * scale)
     
         draw_rectangle(rectangle)
-        draw_rectangle_border(rectangle, thickness = 5 * scale)
         
         inner_rectangle = rectangle.get_inset_rectangle(padding)
         
         elements = self.get_complete_line_amount()[:line_amount]
         for i, line in enumerate(elements):
-            rectangle = Rectangle(inner_rectangle.left, inner_rectangle.top - i*element_height, inner_rectangle.width, real_element_height)
-            draw_rectangle(rectangle, color = (0.2, 0.3, 0.5, 1.0))
+            text_rectangle = Rectangle(inner_rectangle.left, inner_rectangle.top - i*element_height, inner_rectangle.width, real_element_height)
+            if i == selected_index:
+                highlight_rectangle = Rectangle(rectangle.left, rectangle.top - i*element_height - element_height / 4, rectangle.width, element_height)
+                draw_rectangle(highlight_rectangle, color = (0.95, 0.95, 0.95, 1.0))
+            draw_text_on_rectangle(line, text_rectangle, size = text_size)
         
+        draw_rectangle_border(rectangle, thickness = border_thickness)
         restore_opengl_defaults()
         
     def get_complete_line_amount(self):
