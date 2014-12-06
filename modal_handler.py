b119d8068a53ceaf239b74de77799572564abfa7
Jacques Lucke <mail@jlucke.com>
2014-12-06 16:42:46 +0100

    cleanup

--- modal_handler.py
+++ modal_handler.py
@@ -92,37 +92,10 @@ class AutoCompleteTextBox:
         editor_info = TextEditorInfo()
         scale = editor_info.scale
         
-        box_position_info = self.get_operator_box_position_info(editor_info)
-        
+        box_position_info = self.get_operator_box_position_info(editor_info)  
         self.draw_operator_box(box_position_info, operators, scale)
         
-        # text_size = 100 * scale
-        # border_thickness = 3 * scale
-        # padding = 8 * scale
-        
-        # box_rectangle, inner_rectangle, line_rectangles, text_rectangles, description_rectangle = self.get_draw_rectangles(editor_info)
     
-        # draw_rectangle(box_rectangle, color = (0.8, 0.8, 0.8, 0.9))
-        
-        
-        # for i, operator in enumerate(operators):
-            # if i < self.top_index or i > self.bottom_index: continue
-            
-            # index = i - self.top_index
-            # if i == self.selected_index:
-                # draw_rectangle(line_rectangles[index], color = (0.95, 0.95, 0.95, 1))
-            # draw_text_on_rectangle(operator.display_name, text_rectangles[index], size = text_size, align = operator.align)
-        
-        # draw_rectangle_border(box_rectangle, thickness = border_thickness)
-        
-        # if len(operators) > self.selected_index:
-            # operator = operators[self.selected_index]
-            # additional_data = getattr(operator, "additional_data", None)
-            # if isinstance(additional_data, PropertyDocumentation):
-                # self.draw_property_documentation(additional_data, scale)
-            # if isinstance(additional_data, FunctionDocumentation):
-                # self.draw_function_documentation(additional_data, scale)
-        
         restore_opengl_defaults()
     
     def get_operator_box_position_info(self, editor_info):
@@ -189,81 +162,10 @@ class AutoCompleteTextBox:
         position = (rectangle.left, rectangle.bottom + rectangle.height / 4)
         draw_text(text, position, text_size, color = color)
         
-    def draw_property_documentation(self, property, scale):
-        self.draw_documentation_background(scale)
         
-        doc_height = 200 * scale
-        element_height = 28 * scale
-        padding = 8 * scale
-        text_size = 130 * scale
-        
-        draw_text("Type: " + str(property.type), (padding, doc_height - element_height * 1), size = text_size)
-        draw_text("Description: " + str(property.description), (padding, doc_height - element_height * 2), size = text_size)
-        if property.is_readonly:
-            draw_text("Readonly", (padding, doc_height - element_height * 3), size = text_size)
-        if property.type == "Enum":
-            draw_text(str(property.enum_items), ( padding, doc_height - element_height * 4), size = text_size)
-            
-    def draw_function_documentation(self, functions, scale):
-        self.draw_documentation_background(scale)
-        
-        doc_height = 200 * scale
-        element_height = 28 * scale
-        padding = 8 * scale
-        text_size = 130 * scale
-        
-        draw_text("Type: " + str(functions), (padding, doc_height - element_height * 1), size = text_size)
-        
-    def draw_documentation_background(self, scale):
-        region = bpy.context.region
-        
-        doc_width = region.width
-        doc_height = 200 * scale
-        
-        rectangle = Rectangle(0, doc_height, doc_width, doc_height)
-        
-        draw_rectangle(rectangle, color = (0.8, 0.8, 0.8, 0.9))
-        
-    def get_draw_rectangles(self, editor_info):
-        scale = editor_info.scale
-        
-        padding = 8 * scale
-        element_height = 21 * scale
-        real_element_height = 20 * scale
-        box_width = 300 * scale
-        move_down_distance = 10 * scale
-        description_distance = 10 * scale
-        description_width = 500 * scale
-        description_height = 50 * scale
-        
-        x, y = editor_info.cursor_position
-        # the box in which the operators are displayed
-        box_rectangle = Rectangle(x, y, box_width, get_line_amount() * element_height + 2 * padding)
-        box_rectangle.move_down(move_down_distance)
-        
-        # box rectangle with applied padding, to get a few pixels between the text and the border line
-        inner_rectangle = box_rectangle.get_inset_rectangle(padding)
-        
-        line_rectangles = []
-        text_rectangles = []
-        for i in range(get_line_amount()):
-            # rectangle that is drawn to highlight this operator
-            line_rectangles.append(Rectangle(
-                box_rectangle.left,
-                box_rectangle.top - i*element_height - element_height / 4,
-                box_rectangle.width,
-                element_height))
-            # rectangle to draw the text in
-            text_rectangles.append(Rectangle(
-                inner_rectangle.left,
-                inner_rectangle.top - i*element_height,
-                inner_rectangle.width,
-                real_element_height))
-                
-        description_rectangle = Rectangle(box_rectangle.right + description_distance, box_rectangle.top, description_width, description_height)
-                
-        return box_rectangle, inner_rectangle, line_rectangles, text_rectangles, description_rectangle
-      
+    def draw_property_info_box(self, position, property):
+        pass
+   
     @property
     def selected_operator(self):
         operators = get_text_operators()
