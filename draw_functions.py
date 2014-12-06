fc00ddb9d8c993898b1709f916f3d01af7993400
Jacques Lucke <mail@jlucke.com>
2014-12-06 12:57:53 +0100

    calculate drawing lines for text

--- draw_functions.py
+++ draw_functions.py
@@ -42,7 +42,7 @@ def draw_text_on_rectangle(text, rectangle, color = (0.2, 0.2, 0.2, 1.0), size =
 
 def draw_text(text = "", position = (0, 0), size = 20, horizontal_align = "LEFT", vertical_align = "BOTTOM", color = (0.2, 0.2, 0.2, 1.0)):
     glColor4f(*color)
-    blf.size(font_id, int(size), 12)
+    set_text_size(size)
     dimensions = blf.dimensions(font_id, text)
     
     if horizontal_align == "CENTER":
@@ -53,8 +53,35 @@ def draw_text(text = "", position = (0, 0), size = 20, horizontal_align = "LEFT"
     blf.position(font_id, int(position[0]), int(position[1]), 0)
     blf.draw(font_id, text)
     
+def get_text_dimensions(text, size, font_id = font_id):
+    set_text_size(size)
+    return blf.dimensions(font_id, text)
     
+def set_text_size(size, font_id = font_id):
+    blf.size(font_id, int(size), 12)
     
+def get_text_lines(text, size, width, font_id = font_id):
+    lines = []
+    while text != "":
+        next_line = get_next_line(text, size, width, font_id)
+        lines.append(next_line.strip())
+        text = text[(len(next_line)):]
+            
+    return lines
+   
+def get_next_line(text, size, width, font_id = font_id):
+    index = 0
+    fitting_index = 0
+    while index > -1:
+        index = text.find(" ", index + 1)
+        subtext = text[:index]
+        dimensions = get_text_dimensions(subtext, size, font_id)
+        if dimensions[0] <= width:
+            fitting_index = index
+        else:
+            return text[:fitting_index]
+    return text
+   
 def restore_opengl_defaults():
     glLineWidth(1)
     glDisable(GL_BLEND)
