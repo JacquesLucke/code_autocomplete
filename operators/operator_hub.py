c5b68029a77fc205e88ea1ed24d67b238f87226c
Jacques Lucke <mail@jlucke.com>
2014-12-13 11:37:05 +0100

    cleanup

--- operators/operator_hub.py
+++ operators/operator_hub.py
@@ -1,12 +1,10 @@
 from script_auto_complete.operators.extend_word_operators import get_extend_word_operators
 from script_auto_complete.operators.insert_template_operators import get_insert_template_operators
-from script_auto_complete.operators.context_word_operators import get_context_word_operators
 from script_auto_complete.operators.api_context_operators import get_api_context_operators
 
 def get_text_operators():
     operators = []
     operators.extend(get_insert_template_operators())
     operators.extend(get_api_context_operators())
-    operators.extend(get_context_word_operators())
     operators.extend(get_extend_word_operators())
     return operators
