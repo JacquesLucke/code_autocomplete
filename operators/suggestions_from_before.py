60d31b7a204ec2aab2b8809bea5c51b9481e0afa
Jacques Lucke <mail@jlucke.com>
2014-12-13 23:23:19 +0100

    allow ' and "

--- operators/suggestions_from_before.py
+++ operators/suggestions_from_before.py
@@ -23,12 +23,14 @@ def get_suggestion_from_text_before():
     
 suggestions = {}
 
-suggestions[".*bl_space_type.*=.*\""] = [
+suggestions[".*bl_space_type.*=.*(\"|\')"] = [
     "EMPTY", "VIEW_3D", "TIMELINE", "GRAPH_EDITOR", "DOPESHEET", "NLA_EDITOR", "IMAGE_EDITOR", "SEQUENCE_EDITOR", "CLIP_EDITOR",
     "TEXT_EDITOR", "NODE_EDITOR", "LOGIC_EDITOR", "PROPERTIES", "OUTLINER", "USER_PREFERENCES", "INFO", "FILE_BROWSER", "CONSOLE" ]
     
-suggestions[".*bl_region_type.*=.*\""] = ["WINDOW", "HEADER", "CHANNELS", "TEMPORARY", "UI",
+suggestions[".*bl_region_type.*=.*(\"|\')"] = ["WINDOW", "HEADER", "CHANNELS", "TEMPORARY", "UI",
     ("TOOLS", "e.g. left sidebar in 3D view"), ("TOOL_PROPS", "e.g. lower part of the left side part in 3D view"), "PREVIEW"]
+    
+suggestions[".*bl_category.*=.*(\"|\')"] = ["Tools", "Create", "Relations", "Animation", "Physics", "Grease Pencil"]
 
 suggestions["class \w*\("] = ["bpy", "types", "Panel", "Menu", "Operator"]
 suggestions["class \w*\(bpy\."] = ["types"]
