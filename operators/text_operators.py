2a181ec03da698cba067b737f0138ca21ab60d89
Jacques Lucke <mail@jlucke.com>
2014-12-13 11:32:48 +0100

    cleanup

--- operators/text_operators.py
+++ operators/text_operators.py
@@ -1,8 +1,7 @@
 import bpy
 from script_auto_complete.text_editor_utils import *
         
-word_characters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
-random_select_sequence = "!*_*_*!"
+        
 class ExtendWordOperator:
     def __init__(self, target_word, description = "", additional_data = None):
         self.target_word = target_word
@@ -11,16 +10,9 @@ class ExtendWordOperator:
         self.additional_data = additional_data
         self.align = "LEFT"
         
-    def execute(self, text_block):
-        text_line = text_block.current_line
-        character_index = text_block.current_character
-        
-        text = text_line.body
-        word_start_index = get_word_start_index(text, character_index)
-        
-        new_text = text[:word_start_index] + self.target_word + text[character_index:]
-        text_line.body = new_text
-        set_text_cursor_position(text_block.current_line_index, len(text[:word_start_index] + self.target_word))
+    def execute(self, text_block):        
+        word_start = get_word_start()
+        bpy.ops.text.insert(text = self.target_word[len(word_start):])
 
 
 class InsertTextOperator:
