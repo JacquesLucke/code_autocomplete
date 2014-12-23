0dfb6baf19b80dc6a26ca59024c365c7cb71b8f9
Jacques Lucke <mail@jlucke.com>
2014-12-24 00:41:43 +0100

    cleanup

--- operators/bpy_ops_parameter_operators.py
+++ operators/bpy_ops_parameter_operators.py
@@ -10,20 +10,32 @@ def get_bpy_ops_parameter_operators(text_block):
         documentation = get_documentation()
         op = documentation.get_operator_by_full_name(function_path)
         if op is not None:
-            for input in op.inputs:
-                pattern = "[\(\,]\s*"
-                word_start = text_block.get_current_text_after_pattern(pattern)
-                if word_start is not None:
-                    if input.name.upper().startswith(word_start.upper()):
-                        operators.append(ExtendWordOperator(input.name, align = "INSET", additional_data = input))
-                if input.type == "Enum":
-                    pattern = input.name+"\s*=\s*(\"|\')"
-                    word_start = text_block.get_current_text_after_pattern(pattern)
-                    if word_start is None: continue
-                    word_start = word_start.upper()
-                    for suggestion in input.enum_items:
-                        if suggestion.upper().startswith(word_start):
-                            operators.append(ExtendWordOperator(suggestion))
+            operators.extend(get_parameter_name_operators(text_block, op))
+            operators.extend(get_enum_items_operators(text_block, op))
     operators.sort(key = attrgetter("display_name"))
+    return operators
+
+# bpy.ops.text.move(#type# = "NEXT_CHARACTER")
+def get_parameter_name_operators(text_block, op):
+    word_start = text_block.get_current_text_after_pattern("[\(\,]\s*")
+    if word_start is None: return []
+    operators = []
+    word_start = word_start.upper()
+    for input in op.inputs:
+        if input.name.upper().startswith(word_start):
+            operators.append(ExtendWordOperator(input.name, align = "INSET", additional_data = input))
+    return operators
+
+# bpy.ops.text.move(type = "#NEXT_CHARACTER#") 
+def get_enum_items_operators(text_block, op):
+    operators = []
+    for input in op.inputs:
+        pattern = input.name + "\s*=\s*(\"|\')"
+        word_start =text_block.get_current_text_after_pattern(pattern)
+        if word_start is None: continue
+        word_start = word_start.upper()
+        for enum_item in input.enum_items:
+            if enum_item.upper().startswith(word_start):
+                operators.append(ExtendWordOperator(enum_item))
     return operators
     
