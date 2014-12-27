be8700d720e85bb575a351f65c8afcdb7d32f9d6
Jacques Lucke <mail@jlucke.com>
2014-12-27 19:01:43 +0100

    fix

--- operators/operator_hub.py
+++ operators/operator_hub.py
@@ -5,13 +5,13 @@ from script_auto_complete.operators.suggestions_from_before import get_suggestio
 from script_auto_complete.operators.bpy_ops_operators import get_bpy_ops_operators
 from script_auto_complete.operators.parameter_operators import get_parameter_operators
 from script_auto_complete.operators.dynamic_snippets_operators import get_dynamic_snippets_operators
-from script_auto_complete.operators.assign_value_operators import get_assign_value_operators
+from script_auto_complete.operators.assign_or_compare_operators import get_assign_or_compare_operators
 
 def get_text_operators(text_block):
     operators = []
     operators.extend(get_insert_template_operators(text_block))
     operators.extend(get_dynamic_snippets_operators(text_block))
-    operators.extend(get_assign_value_operators(text_block))
+    operators.extend(get_assign_or_compare_operators(text_block))
     operators.extend(get_parameter_operators(text_block))
     operators.extend(get_bpy_ops_operators(text_block))    
     operators.extend(get_suggestion_from_text_before(text_block))
