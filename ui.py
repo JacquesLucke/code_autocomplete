0c8af0500a924775d35248a94d96c7aa2577edf4
Jacques Lucke <mail@jlucke.com>
2015-01-17 17:21:21 +0100

    solce whitespace inconsitency button

--- ui.py
+++ ui.py
@@ -19,10 +19,12 @@ class AutoCompleteSettingsPanel(bpy.types.Panel):
         
     def draw(self, context):
         layout = self.layout
-        if running: layout.operator("script_auto_complete.stop_auto_completion", icon = "PANEL_CLOSE")
+        if running: 
+            layout.operator("script_auto_complete.stop_auto_completion", icon = "PANEL_CLOSE")
         else: layout.operator("script_auto_complete.start_auto_completion", icon = "LIBRARY_DATA_DIRECT")
         if get_documentation().is_build:
             layout.operator("script_auto_complete.rebuild_documentation")
+        layout.operator("script_auto_complete.correct_whitespaces")
         
 
 class StartAutoCompletion(bpy.types.Operator):
@@ -34,7 +36,7 @@ class StartAutoCompletion(bpy.types.Operator):
         return not running
     
     def modal(self, context, event):
-        if not running:
+        if not running or event.type == "F8":
             self.modal_handler.free()
             return { "FINISHED" }
     
@@ -78,4 +80,16 @@ class StopAutoCompletion(bpy.types.Operator):
     
     def execute(self, context):
         stop()
-        return { "FINISHED" }        
+        return { "FINISHED" }   
+
+class SolveWhitespaceInconsistency(bpy.types.Operator):
+    bl_idname = "script_auto_complete.correct_whitespaces"
+    bl_label = "Correct Whitespaces"
+    bl_description = "Convert whitespaces to spaces or tabs"
+    
+    def execute(self, context):
+        if context.edit_text.use_tabs_as_spaces:
+            bpy.ops.text.convert_whitespace(type = "TABS")
+        else:
+            bpy.ops.text.convert_whitespace(type = "SPACES")
+        return { "FINISHED" } 
