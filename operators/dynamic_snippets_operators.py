cdf155929f562d4f581c69fc9db6ca2cd804d522
Jacques Lucke <mail@jlucke.com>
2015-05-06 18:09:16 +0200

    deduplicate template insertion through dynamic snippets

--- operators/dynamic_snippets_operators.py
+++ operators/dynamic_snippets_operators.py
@@ -58,231 +58,55 @@ class NewClassSnippet:
 class NewPanelSnippet:
     expression = "class (\w*)\(.*Panel\):"
     
-    panel_code = '''
-    bl_idname = "IDNAME"
-    bl_label = "LABEL"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "TOOLS"
-    bl_category = "category"
-    
-    def draw(self, context):
-        layout = self.layout
-        '''
-    
     def insert_snippet(self, text_block, match, name):
-        code = self.panel_code
-        code = code.replace("IDNAME", self.get_idname(match))
-        code = code.replace("LABEL", self.get_label(match))
-        text_block.insert(code)
+        text_block.current_line = ""
+        bpy.ops.code_autocomplete.insert_panel(class_name = match.group(1))
         
     def get_snippet_names(self, match):
         return ["New Panel"]
-        
-    def get_idname(self, match):
-        name = match.group(1)
-        return to_lower_case_with_underscores(name)  
-        
-    def get_label(self, match):
-        name = match.group(1)
-        return to_ui_text(name) 
         
         
 class NewMenuSnippet:
     expression = "class (\w*)\(.*Menu\):"
     
-    menu_code = '''
-    bl_idname = "IDNAME"
-    bl_label = "LABEL"
+    menu_types_dict = {
+        "New Menu" : "NORMAL",
+        "New Pie Menu" : "PIE" }
     
-    def draw(self, context):
-        layout = self.layout
-        '''
-    
-    pie_menu_code = '''
-    bl_idname = "IDNAME"
-    bl_label = "LABEL"
-    
-    def draw(self, context):
-        pie = self.layout.menu_pie()
-        '''
-      
     def insert_snippet(self, text_block, match, name):
-        if name == "New Menu": code = self.menu_code
-        if name == "New Pie Menu": code = self.pie_menu_code
-        
-        code = code.replace("IDNAME", self.get_idname(match))
-        code = code.replace("LABEL", self.get_label(match))
-        text_block.insert(code)
+        text_block.current_line = ""
+        bpy.ops.code_autocomplete.insert_menu(
+            class_name = match.group(1), 
+            menu_type = self.menu_types_dict[name])
         
     def get_snippet_names(self, match):
-        return ["New Menu", "New Pie Menu"]
-        
-    def get_idname(self, match):
-        name = match.group(1)
-        return "view3D." + to_lower_case_with_underscores(name)  
-        
-    def get_label(self, match):
-        name = match.group(1)
-        return to_ui_text(name) 
+        return self.menu_types_dict.keys()
       
         
 class NewOperatorSnippet:
     expression = "class (\w*)\(.*Operator\):"
     
-    operator_code = '''
-    bl_idname = "IDNAME"
-    bl_label = "LABEL"
-    bl_description = ""
-    bl_options = {"REGISTER"}
-    
-    @classmethod
-    def poll(cls, context):
-        return True
-    
-    def execute(self, context):
-        return {"FINISHED"}
-        '''
-        
-    modal_operator_code = '''
-    bl_idname = "IDNAME"
-    bl_label = "LABEL"
-    bl_description = ""
-    bl_options = {"REGISTER"}
-    
-    @classmethod
-    def poll(cls, context):
-        return True
-        
-    def modal(self, context, event):
-    
-        if event.type == "LEFTMOUSE":
-            return {"FINISHED"}
-    
-        if event.type in {"RIGHTMOUSE", "ESC"}:
-            return {"CANCELLED"}
-            
-        return {"RUNNING_MODAL"}
-    
-    def invoke(self, context, event):
-        context.window_manager.modal_handler_add(self)
-        return {"RUNNING_MODAL"}
-        '''
-        
-    modal_operator_draw_code = '''
-    bl_idname = "IDNAME"
-    bl_label = "LABEL"
-    bl_description = ""
-    bl_options = {"REGISTER"}
-    
-    @classmethod
-    def poll(cls, context):
-        return True
-        
-    def invoke(self, context, event):
-        args = (self, context)
-        self._handle = bpy.types.SpaceView3D.draw_handler_add(self.draw_callback_px, args, "WINDOW", "POST_PIXEL")
-        context.window_manager.modal_handler_add(self)
-        return {"RUNNING_MODAL"}
-        
-    def modal(self, context, event):
-        context.area.tag_redraw()
-        
-        if event.type == "LEFTMOUSE":
-            self.cancel(context)
-            return {"FINISHED"}
-            
-        if event.type in {"RIGHTMOUSE", "ESC"}:
-            self.cancel(context)
-            return {"CANCELLED"}
-            
-        return {"RUNNING_MODAL"}
-        
-    def cancel(self, context):
-        bpy.types.SpaceView3D.draw_handler_remove(self._handle, "WINDOW")
-    
-    def draw_callback_px(tmp, self, context):
-        pass
-    '''
+    operator_types_dict = {
+        "New Operator" : "NORMAL",
+        "New Modal Operator" : "MODAL",
+        "New Modal Operator Draw" : "MODAL_DRAW"}
     
     def insert_snippet(self, text_block, match, name):
-        if name == "New Operator": code = self.operator_code
-        if name == "New Modal Operator": code = self.modal_operator_code
-        if name == "New Modal Operator Draw": code = self.modal_operator_draw_code
-        
-        code = code.replace("IDNAME", self.get_idname(match))
-        code = code.replace("LABEL", self.get_label(match))
-        text_block.insert(code)
+        text_block.current_line = ""
+        bpy.ops.code_autocomplete.insert_operator(
+            class_name = match.group(1), 
+            operator_type = self.operator_types_dict[name])
         
     def get_snippet_names(self, match):
-        return ["New Operator", "New Modal Operator", "New Modal Operator Draw"]
-        
-    def get_idname(self, match):
-        name = match.group(1)
-        return "my_operators." + to_lower_case_with_underscores(name)  
-        
-    def get_label(self, match):
-        name = match.group(1)
-        return to_ui_text(name) 
-        
-
-def to_lower_case_with_underscores(name):
-    words = get_words_from_variable(name)
-    words = [word.lower() for word in words]
-    output = "_".join(words)
-    return output  
-    
-def to_ui_text(name):
-    words = get_words_from_variable(name)
-    words = [word.capitalize() for word in words]
-    output = " ".join(words)
-    return output      
-        
-def get_words_from_variable(name):
-    words = []
-    current_word = ""
-    for char in name:
-        if char.islower():
-            current_word += char
-        if char.isupper():
-            if current_word.isupper() or len(current_word) == 0:
-                current_word += char
-            else:
-                words.append(current_word)
-                current_word = char
-        if char == "_":
-            words.append(current_word)
-            current_word = ""
-            
-    words.append(current_word)
-    words = [word for word in words if len(word) > 0]
-    return words  
+        return sorted(self.operator_types_dict.keys(), key = lambda s: len(s))
     
         
 class SetupKeymapsSnippet:
     expression = "=keymaps"
     
-    keymap_registration_code = '''addon_keymaps = []
-def register_keymaps():
-    global addon_keymaps
-    wm = bpy.context.window_manager
-    km = wm.keyconfigs.addon.keymaps.new(name = "3D View", space_type = "VIEW_3D")
-    
-    addon_keymaps.append(km)
-    
-def unregister_keymaps():
-    global addon_keymaps
-    wm = bpy.context.window_manager
-    for km in addon_keymaps:
-        for kmi in km.keymap_items:
-            km.keymap_items.remove(kmi)
-        wm.keyconfigs.addon.keymaps.remove(km)
-    addon_keymaps.clear()
-'''
-    
     def insert_snippet(self, text_block, match, name):
-        text_block.select_match_in_current_line(match)
-        text_block.delete_selection()
-        text_block.insert(self.keymap_registration_code)
+        text_block.current_line = ""
+        bpy.ops.code_autocomplete.insert_keymap()
     
     def get_snippet_names(self, match):
         return ["Keymap Registration"]
