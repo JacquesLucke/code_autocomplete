5fbc5fa27ba373ebcee70e8782d5e4c26c36bdd3
Jacques Lucke <mail@jlucke.com>
2015-05-06 18:31:46 +0200

    increase version number

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ bl_info = {
     "name":        "Code Autocomplete",
     "description": "Improve the scripting experience in Blenders text editor.",
     "author":      "Jacques Lucke",
-    "version":     (1, 5, 0),
+    "version":     (1, 5, 1),
     "blender":     (2, 7, 4),
     "location":    "Text Editor",
     "category":    "Development"
