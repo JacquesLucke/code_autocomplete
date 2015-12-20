3132c25aa89a2f9437b45cb77e9cc27c22e14c5e
Jacques Lucke <mail@jlucke.com>
2015-12-20 21:26:40 +0100

    more static pattern completions

--- autocompletion/suggestions/static_pattern_completion.py
+++ autocompletion/suggestions/static_pattern_completion.py
@@ -1,4 +1,5 @@
 import re
+import bpy
 from . interface import Provider, Completion
 
 
@@ -19,8 +20,8 @@ def iter_static_completions(text_block):
         if word_start is None: continue
         word_start = word_start.upper()
         for word in words:
-            if word_start in word:
-                    yield WordCompletion(word)
+            if word_start in word.upper():
+                yield WordCompletion(word)
 
 space_types = [
     "EMPTY", "VIEW_3D", "TIMELINE", "GRAPH_EDITOR", "DOPESHEET", "NLA_EDITOR",
@@ -29,10 +30,20 @@ space_types = [
 
 region_types = ["WINDOW", "HEADER", "CHANNELS", "TEMPORARY", "UI", "TOOLS", "TOOL_PROPS", "PREVIEW"]
 
+handlers = [
+    "frame_change_post", "frame_change_post", "frame_change_pre",
+    "game_pre", "load_post", "load_pre", "render_cancel", "render_complete",
+    "render_init", "render_post", "render_pre", "render_stats", "render_write",
+    "save_post", "save_pre", "scene_update_post", "scene_update_pre", "version_update"]
+
 suggestions = {
     "bl_space_type\s*=\s*(\"|\')" : space_types,
     "bl_region_type\s*=\s*(\"|\')" : space_types,
     "bl_options\s*=.*(,|{)\s*(\"|\')" : ["REGISTER", "UNDO", "BLOCKING", "GRAB_POINTER", "PRESET", "INTERNAL"],
     "bl_category\s*=\s*(\"|\')" : ["Tools", "Create", "Relations", "Animation", "Physics", "Grease Pencil"],
-    "return\s*\{\s*(\"|\')" : ["RUNNING_MODAL", "CANCELLED", "FINISHED", "PASS_THROUGH"]
+    "return\s*\{\s*(\"|\')" : ["RUNNING_MODAL", "CANCELLED", "FINISHED", "PASS_THROUGH"],
+    "bpy\." : ["context", "data", "ops", "types", "utils", "path", "app", "props"],
+    "bpy\.app\." : ["handlers", "translations"],
+    "bpy\.app\.handlers\." : handlers,
+    "bpy\.props\." : [type_name for type_name in dir(bpy.props) if type_name[0] != "_"]
 }
