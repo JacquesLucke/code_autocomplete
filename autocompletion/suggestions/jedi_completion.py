c53e962e13a9ecba04ff4028681411a64317f1c9
Jacques Lucke <mail@jlucke.com>
2015-12-21 22:11:48 +0100

    new preprocessing of the code before giving it to jedi

--- autocompletion/suggestions/jedi_completion.py
+++ autocompletion/suggestions/jedi_completion.py
@@ -30,28 +30,39 @@ class JediCompletionProvider(Provider):
             return []
 
 def get_completion_source(text_block):
-    text = text_block.text
-    text = replace_import_statements(text)
-    text = append_clarification_lines(text)
-    current_line = text_block.text_before_cursor
-    current_line = replace_import_statements(current_line)
-
-    line_index = text_block.current_line_index + 1
-    character_index = len(current_line)
+    current_line_number = text_block.current_line_index + 1
+    new_current_line_number = None
+    character_index = len(text_block.text_before_cursor)
     filepath = text_block.filepath
-    return text, line_index, character_index, filepath
 
-def replace_import_statements(text):
-    text = text.replace("import bpy", "import {} as bpy".format(fake_package_name))
-    text = re.sub("from bpy", "from {}".format(fake_package_name), text)
-    return text
+    new_lines = []
+    for i, line in enumerate(text_block.iter_lines()):
+        line_number = i + 1
+        new_lines.extend(list(iter_corrected_lines_from_line(line)))
+        if line_number == current_line_number:
+            new_current_line_number = len(new_lines)
 
-def append_clarification_lines(text):
-    return text + "\n" + "\n".join(iter_clarification_lines(text))
+    text = "\n".join(new_lines)
+    return text, new_current_line_number, character_index, filepath
 
-def iter_clarification_lines(text):
-    for match in re.finditer("class (\w+)\(.*Operator\)", text):
-        yield "{}().invoke(bpy.context, bpy.__private__.event.Event())".format(match.group(1))
-        yield "{}().execute(bpy.context)".format(match.group(1))
-    for match in re.finditer("class (\w+)\(.*Panel\)", text):
-        yield "{}().draw(bpy.context)".format(match.group(1))
+def iter_corrected_lines_from_line(line):
+    if "bpy" in line:
+        line = line.replace("import bpy", "import {} as bpy".format(fake_package_name))
+        line = line.replace("from bpy", "from {}".format(fake_package_name))
+    yield line
+
+    if "def draw(self, context):" in line:
+        indentation = line.index("d") + 4
+        yield " " * indentation + "context = bpy.__private__.context.Context()"
+        yield " " * indentation + "self.layout = bpy.__private__.uilayout.UILayout()"
+
+    if "def execute(self, context):" in line or \
+       "def poll(cls, context):" in line:
+        indentation = line.index("d") + 4
+        yield " " * indentation + "context = bpy.__private__.context.Context()"
+
+    if "def invoke(self, context, event):" in line or \
+       "def modal(self, context, event):" in line:
+        indentation = line.index("d") + 4
+        yield " " * indentation + "context = bpy.__private__.context.Context()"
+        yield " " * indentation + "event = bpy.__private__.event.Event()"
