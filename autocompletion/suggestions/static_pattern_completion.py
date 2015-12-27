e4dfd3b70e170e31ea3309ea71c1459c64ef0741
Jacques Lucke <mail@jlucke.com>
2015-12-27 21:46:25 +0100

    cleanup static pattern completion

--- autocompletion/suggestions/static_pattern_completion.py
+++ autocompletion/suggestions/static_pattern_completion.py
@@ -19,31 +19,36 @@ def iter_static_completions(text_block):
         word_start = text_block.get_current_text_after_pattern(pattern)
         if word_start is None: continue
         word_start = word_start.upper()
-        for word in words:
-            if word_start in word.upper():
-                yield WordCompletion(word)
 
-space_types = [
-    "EMPTY", "VIEW_3D", "TIMELINE", "GRAPH_EDITOR", "DOPESHEET", "NLA_EDITOR",
-    "IMAGE_EDITOR", "SEQUENCE_EDITOR", "CLIP_EDITOR", "TEXT_EDITOR", "NODE_EDITOR",
-    "LOGIC_EDITOR", "PROPERTIES", "OUTLINER", "USER_PREFERENCES", "INFO", "FILE_BROWSER", "CONSOLE"]
+        secondaryCompletions = []
+        for word in words:
+            if word.upper().startswith(word_start):
+                yield WordCompletion(word)
+            elif word_start in word.upper():
+                secondaryCompletions.append(WordCompletion(word))
+        yield from secondaryCompletions
+
+space_properties = bpy.types.Space.bl_rna.properties
+space_types = sorted([item.identifier for item in space_properties["type"].enum_items])
 
 region_types = ["WINDOW", "HEADER", "CHANNELS", "TEMPORARY", "UI", "TOOLS", "TOOL_PROPS", "PREVIEW"]
 
-handlers = [
-    "frame_change_post", "frame_change_post", "frame_change_pre",
-    "game_pre", "load_post", "load_pre", "render_cancel", "render_complete",
-    "render_init", "render_post", "render_pre", "render_stats", "render_write",
-    "save_post", "save_pre", "scene_update_post", "scene_update_pre", "version_update"]
+handlers = [name for name in dir(bpy.app.handlers) if not name.startswith("_")]
+
+event_properties = bpy.types.Event.bl_rna.properties
+event_types = sorted([item.identifier for item in event_properties["type"].enum_items])
+event_values = sorted([item.identifier for item in event_properties["value"].enum_items])
 
 suggestions = {
     "bl_space_type\s*=\s*(\"|\')" : space_types,
     "bl_region_type\s*=\s*(\"|\')" : region_types,
-    "bl_options\s*=.*(,|{)\s*(\"|\')" : ["REGISTER", "UNDO", "BLOCKING", "GRAB_POINTER", "PRESET", "INTERNAL"],
+    "bl_options\s*=.*(,|{)\s*(\"|\')" : ["REGISTER", "UNDO", "BLOCKING", "GRAB_POINTER", "PRESET", "INTERNAL", "DEFAULT_CLOSED", "HIDE_HEADER"],
     "bl_category\s*=\s*(\"|\')" : ["Tools", "Create", "Relations", "Animation", "Physics", "Grease Pencil"],
     "return\s*\{\s*(\"|\')" : ["RUNNING_MODAL", "CANCELLED", "FINISHED", "PASS_THROUGH"],
     "bpy\." : ["context", "data", "ops", "types", "utils", "path", "app", "props"],
     "bpy\.app\." : ["handlers", "translations"],
     "bpy\.app\.handlers\." : handlers,
-    "bpy\.props\." : [type_name for type_name in dir(bpy.props) if type_name[0] != "_"]
+    "bpy\.props\." : [type_name for type_name in dir(bpy.props) if type_name[0] != "_"],
+    "keymap_items\.new\(.*, type = (\"|\')" : event_types,
+    "keymap_items\.new\(.*, value = (\"|\')" : event_values
 }
